791aaa87bc3d5da37b59e11e33e298d3337d386a
Rafi4 <Blood.wars12@gmail.com>
2023-11-17 19:54:41 +0100

    add new marker

--- lib/main.dart
+++ lib/main.dart
@@ -1,85 +1,59 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'get_api.dart';
+import 'package:google_maps_rest_api/get_api.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(const MyApp());
 
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
-  State<MyApp> createState() => _MyAppState();
+  _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
   final Map<String, Marker> _markers = {};
-  late GoogleMapController mapController; // Controller for Google map
 
-  void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
-    _fetchAndSetMarkers();
+  Future<void> _fetchAndSetMarkers() async {
+    final List<dynamic> stations = await fetchStations();
+    setState(() {
+      _markers.clear();
+      for (final station in stations) {
+        final marker = Marker(
+          markerId: MarkerId(station['id'].toString()),
+          position: LatLng(double.parse(station['gegrLat']), double.parse(station['gegrLon'])),
+          infoWindow: InfoWindow(
+            title: station['stationName'],
+            snippet: 'ID: ${station['id']}',
+          ),
+        );
+        _markers[station['id'].toString()] = marker;
+      }
+    });
   }
 
-Future<void> _fetchAndSetMarkers() async {
-  final List<dynamic> stations = await fetchStations();
-  setState(() {
-    _markers.clear();
-    for (var station in stations) {
-      fetchStationSensors(station['id']).then((sensors) {
-        var pm10Sensor = sensors.firstWhere(
-          (sensor) => sensor['param']['paramCode'] == 'PM10',
-          orElse: () => null,
-        );
-        if (pm10Sensor != null) {
-          fetchSensorData(pm10Sensor['id']).then((data) {
-            // Przykład zakłada, że interesuje nas najnowsza wartość PM10.
-            var pm10Value = data['values'].firstWhere(
-              (v) => v['value'] != null,
-              orElse: () => {'value': 'Brak danych'},
-            )['value'];
-
-            final marker = Marker(
-              markerId: MarkerId(station['id'].toString()),
-              position: LatLng(
-                double.tryParse(station['gegrLat']) ?? 0.0,
-                double.tryParse(station['gegrLon']) ?? 0.0,
-              ),
-              infoWindow: InfoWindow(
-                title: station['stationName'] ?? 'Unknown',
-                snippet: 'PM10: $pm10Value',
-              ),
-            );
-
-            setState(() {
-              _markers[station['id'].toString()] = marker;
-            });
-          });
-        }
-      });
-    }
-  });
-}
-
+  @override
+  void initState() {
+    super.initState();
+    _fetchAndSetMarkers();
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Stations on Map'),
-          backgroundColor: Colors.green[700],
+      home: GoogleMap(
+        onMapCreated: (GoogleMapController controller) {},
+        initialCameraPosition: const CameraPosition(
+          target: LatLng(52.237049, 21.017532), // Przykładowe współrzędne środkowe dla Polski
+          zoom: 5,
         ),
-        body: GoogleMap(
-          onMapCreated: _onMapCreated,
-          initialCameraPosition: const CameraPosition(
-            target: LatLng(52.237049, 21.017532), // Central point in Poland
-            zoom: 5,
-          ),
-          markers: _markers.values.toSet(),
-        ),
+        markers: _markers.values.toSet(),
       ),
     );
   }
+
+  // Tu wstaw funkcję fetchStations(), którą wcześniej zdefiniowałeś
 }
 
-void main() {
-  runApp(const MyApp());
-}
