adb8633b8b65a55db0eb48ab9f67bb910facbf46
Rafi4 <Blood.wars12@gmail.com>
2023-12-20 22:01:43 +0100

    Further work on gauge UI integration.

--- lib/maps.dart
+++ lib/maps.dart
@@ -3,9 +3,10 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:syncfusion_flutter_gauges/gauges.dart';
+
 import 'block/location_gps.dart';
 import 'block/marker_cubit.dart';
+import 'marker/marker_helper.dart';
 import 'shared preferences/map_service.dart';
 import 'shared preferences/preferences_service.dart';
 import 'package:logger/logger.dart';
@@ -22,8 +23,10 @@ class MyAppState extends State<MyApp> {
   late GoogleMapController _controller;
   MapType _currentMapType = MapType.normal;
 
-  double _todayValue = 250;
+//double _todayValue = 250;
  bool _isMarkerVisible = false;
+
+ //final markerHelper = const MarkerHelperUdate();
 
   void _onMapTypeButtonPressed() {
     setState(() {
@@ -32,14 +35,6 @@ class MyAppState extends State<MyApp> {
           : MapType.normal;
     });
   }
-
-  //    void _toggleVisibility() {
-  //   setState(() {
-  //    _isMarkerVisible = !_isMarkerVisible;
-  //   });
-  // }
-
-
   Future<void> _printSavedCameraPosition() async {
     final prefs = await SharedPreferences.getInstance();
     double? latitude = prefs.getDouble('latitude');
@@ -55,10 +50,6 @@ class MyAppState extends State<MyApp> {
     _controller.animateCamera(CameraUpdate.newCameraPosition(position));
   }
 
- 
-
-
-  
 
   @override
   Widget build(BuildContext context) {
@@ -111,10 +102,9 @@ class MyAppState extends State<MyApp> {
                             ),
                           ],
                         ),
-                        child: Padding(
-                          padding: const EdgeInsets.all(8.0),
-                          child:
-                              buildSleepQualityGauge(), // Call the chart widget here
+                        child: const Padding(
+                          padding: EdgeInsets.all(8.0),
+                          child: MarkerHelperUdate(),
                         ),
                       ),
                     ),
@@ -158,300 +148,5 @@ class MyAppState extends State<MyApp> {
         },
       ),
     ));
-  }
-
-  Widget buildSleepQualityGauge() {
-    final Brightness brightness = Theme.of(context).brightness;
-    return Column(
-      children: [
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const Text(
-                  'TODAY',
-                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500),
-                ),
-                Text(
-                  _todayValue.toStringAsFixed(0),
-                  style: TextStyle(
-                      fontSize: 26,
-                      color: _todayValue < 200
-                          ? Colors.green
-                          : _todayValue < 300
-                              ? Colors.amber
-                              : _todayValue < 400
-                                  ? const Color(0xffFB7D55)
-                                  : Colors.red,
-                      fontWeight: FontWeight.bold),
-                )
-              ],
-            ),
-            Expanded(
-              child: SfLinearGauge(
-                  minimum: 100.0,
-                  maximum: 500.0,
-                  interval: 50.0,
-                  animateAxis: true,
-                  animateRange: true,
-                  showLabels: false,
-                  showTicks: false,
-                  minorTicksPerInterval: 0,
-                  axisTrackStyle: LinearAxisTrackStyle(
-                    thickness: 15,
-                    color: brightness == Brightness.dark
-                        ? Colors.transparent
-                        : Colors.grey[350],
-                  ),
-                  markerPointers: <LinearMarkerPointer>[
-                    LinearShapePointer(
-                        value: _todayValue,
-                        onChanged: (dynamic value) {
-                          setState(() {
-                            _todayValue = value as double;
-                          });
-                        },
-                        height: 20,
-                        width: 20,
-                        color: _todayValue < 200
-                            ? Colors.green
-                            : _todayValue < 300
-                                ? Colors.amber
-                                : _todayValue < 400
-                                    ? const Color(0xffFB7D55)
-                                    : Colors.red,
-                        position: LinearElementPosition.cross,
-                        shapeType: LinearShapePointerType.circle),
-                    const LinearWidgetPointer(
-                      value: 150,
-                      enableAnimation: true,
-                      position: LinearElementPosition.outside,
-                      offset: 4,
-                      child: Text(
-                        'Doskonała',
-                        style: TextStyle(
-                          fontSize: 12,
-                        ),
-                      ),
-                    ),
-                    const LinearWidgetPointer(
-                      value: 250,
-                      enableAnimation: true,
-                      position: LinearElementPosition.outside,
-                      offset: 4,
-                      child: Text(
-                        'Dobra',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    ),
-                    const LinearWidgetPointer(
-                      value: 350,
-                      enableAnimation: true,
-                      position: LinearElementPosition.outside,
-                      offset: 4,
-                      child: Text(
-                        'Umiarkowana',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    ),
-                    const LinearWidgetPointer(
-                      value: 450,
-                      enableAnimation: true,
-                      position: LinearElementPosition.outside,
-                      offset: 4,
-                      child: Text(
-                        'Zła',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    )
-                  ],
-                  ranges: const <LinearGaugeRange>[
-                    LinearGaugeRange(
-                      startValue: 100.0,
-                      endValue: 200,
-                      startWidth: 8,
-                      midWidth: 8,
-                      endWidth: 8,
-                      position: LinearElementPosition.cross,
-                      color: Colors.green,
-                    ),
-                    LinearGaugeRange(
-                      startValue: 200.0,
-                      endValue: 300,
-                      startWidth: 8,
-                      position: LinearElementPosition.cross,
-                      midWidth: 8,
-                      endWidth: 8,
-                      color: Colors.amber,
-                    ),
-                    LinearGaugeRange(
-                      startValue: 300.0,
-                      endValue: 400,
-                      position: LinearElementPosition.cross,
-                      startWidth: 8,
-                      midWidth: 8,
-                      endWidth: 8,
-                      color: Color(0xffFB7D55),
-                    ),
-                    LinearGaugeRange(
-                      startValue: 400.0,
-                      endValue: 500,
-                      position: LinearElementPosition.cross,
-                      startWidth: 8,
-                      midWidth: 8,
-                      endWidth: 8,
-                      color: Colors.red,
-                    ),
-                  ]),
-            ),
-          ],
-        ),
-        Row(
-          children: [
-            Column(
-              //   crossAxisAlignment: CrossAxisAlignment.start,
-              children: <Widget>[
-                const Text(
-                  'TODAY',
-                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500),
-                ),
-                Text(
-                  _todayValue.toStringAsFixed(0),
-                  style: TextStyle(
-                      fontSize: 26,
-                      color: _todayValue < 200
-                          ? Colors.green
-                          : _todayValue < 300
-                              ? Colors.amber
-                              : _todayValue < 400
-                                  ? const Color(0xffFB7D55)
-                                  : Colors.red,
-                      fontWeight: FontWeight.bold),
-                )
-              ],
-            ),
-            SizedBox(
-              child: SfLinearGauge(
-                  minimum: 100.0,
-                  maximum: 500.0,
-                  interval: 50.0,
-                  animateAxis: true,
-                  animateRange: true,
-                  showLabels: false,
-                  showTicks: false,
-                  minorTicksPerInterval: 0,
-                  axisTrackStyle: LinearAxisTrackStyle(
-                    thickness: 15,
-                    color: brightness == Brightness.dark
-                        ? Colors.transparent
-                        : Colors.grey[350],
-                  ),
-                  markerPointers: <LinearMarkerPointer>[
-                    LinearShapePointer(
-                        value: _todayValue,
-                        onChanged: (dynamic value) {
-                          setState(() {
-                            _todayValue = value as double;
-                          });
-                        },
-                        height: 20,
-                        width: 20,
-                        color: _todayValue < 200
-                            ? Colors.green
-                            : _todayValue < 300
-                                ? Colors.amber
-                                : _todayValue < 400
-                                    ? const Color(0xffFB7D55)
-                                    : Colors.red,
-                        position: LinearElementPosition.cross,
-                        shapeType: LinearShapePointerType.circle),
-                    const LinearWidgetPointer(
-                      value: 150,
-                      enableAnimation: true,
-                      position: LinearElementPosition.outside,
-                      offset: 4,
-                      child: Text(
-                        'Doskonała',
-                        style: TextStyle(
-                          fontSize: 12,
-                        ),
-                      ),
-                    ),
-                    const LinearWidgetPointer(
-                      value: 250,
-                      enableAnimation: true,
-                      position: LinearElementPosition.outside,
-                      offset: 4,
-                      child: Text(
-                        'Dobra',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    ),
-                    const LinearWidgetPointer(
-                      value: 350,
-                      enableAnimation: true,
-                      position: LinearElementPosition.outside,
-                      offset: 4,
-                      child: Text(
-                        'Umiarkowana',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    ),
-                    const LinearWidgetPointer(
-                      value: 450,
-                      enableAnimation: true,
-                      position: LinearElementPosition.outside,
-                      offset: 4,
-                      child: Text(
-                        'Zła',
-                        style: TextStyle(fontSize: 12),
-                      ),
-                    )
-                  ],
-                  ranges: const <LinearGaugeRange>[
-                    LinearGaugeRange(
-                      startValue: 100.0,
-                      endValue: 200,
-                      startWidth: 8,
-                      midWidth: 8,
-                      endWidth: 8,
-                      position: LinearElementPosition.cross,
-                      color: Colors.green,
-                    ),
-                    LinearGaugeRange(
-                      startValue: 200.0,
-                      endValue: 300,
-                      startWidth: 8,
-                      position: LinearElementPosition.cross,
-                      midWidth: 8,
-                      endWidth: 8,
-                      color: Colors.amber,
-                    ),
-                    LinearGaugeRange(
-                      startValue: 300.0,
-                      endValue: 400,
-                      position: LinearElementPosition.cross,
-                      startWidth: 8,
-                      midWidth: 8,
-                      endWidth: 8,
-                      color: Color(0xffFB7D55),
-                    ),
-                    LinearGaugeRange(
-                      startValue: 400.0,
-                      endValue: 500,
-                      position: LinearElementPosition.cross,
-                      startWidth: 8,
-                      midWidth: 8,
-                      endWidth: 8,
-                      color: Colors.red,
-                    ),
-                  ]),
-            ),
-          ],
-        ),
-      ],
-    );
   }
 }
--- lib/marker/marker_helper.dart
+++ lib/marker/marker_helper.dart
@@ -1,8 +1,319 @@
 //import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:syncfusion_flutter_gauges/gauges.dart';
 import 'marker_icon_loader.dart';
 
+class MarkerHelperUdate extends StatefulWidget {
+ 
+  const MarkerHelperUdate({super.key});
+
+  @override
+  State<MarkerHelperUdate> createState() => _MarkerHelperUdateState();
+}
+class _MarkerHelperUdateState extends State<MarkerHelperUdate> {
+  double _todayValue = 250;
+
+    @override
+  Widget build(BuildContext context) {
+    return buildSleepQualityGauge();
+  }
+
+  Widget buildSleepQualityGauge() {
+   final Brightness brightness = Theme.of(context).brightness;
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  'TODAY',
+                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  _todayValue.toStringAsFixed(0),
+                  style: TextStyle(
+                      fontSize: 26,
+                      color: _todayValue < 200
+                          ? Colors.green
+                          : _todayValue < 300
+                              ? Colors.amber
+                              : _todayValue < 400
+                                  ? const Color(0xffFB7D55)
+                                  : Colors.red,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Expanded(
+              child: SfLinearGauge(
+                  minimum: 100.0,
+                  maximum: 500.0,
+                  interval: 50.0,
+                  animateAxis: true,
+                  animateRange: true,
+                  showLabels: false,
+                  showTicks: false,
+                  minorTicksPerInterval: 0,
+                  axisTrackStyle: LinearAxisTrackStyle(
+                    thickness: 15,
+                    color: brightness == Brightness.dark
+                        ? Colors.transparent
+                        : Colors.grey[350],
+                  ),
+                  markerPointers: <LinearMarkerPointer>[
+                    LinearShapePointer(
+                        value: _todayValue,
+                        onChanged: (dynamic value) {
+                          setState(() {
+                            _todayValue = value as double;
+                          });
+                        },
+                        height: 20,
+                        width: 20,
+                        color: _todayValue < 200
+                            ? Colors.green
+                            : _todayValue < 300
+                                ? Colors.amber
+                                : _todayValue < 400
+                                    ? const Color(0xffFB7D55)
+                                    : Colors.red,
+                        position: LinearElementPosition.cross,
+                        shapeType: LinearShapePointerType.circle),
+                    const LinearWidgetPointer(
+                      value: 150,
+                      enableAnimation: true,
+                      position: LinearElementPosition.outside,
+                      offset: 4,
+                      child: Text(
+                        'Doskonała',
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    const LinearWidgetPointer(
+                      value: 250,
+                      enableAnimation: true,
+                      position: LinearElementPosition.outside,
+                      offset: 4,
+                      child: Text(
+                        'Dobra',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    ),
+                    const LinearWidgetPointer(
+                      value: 350,
+                      enableAnimation: true,
+                      position: LinearElementPosition.outside,
+                      offset: 4,
+                      child: Text(
+                        'Umiarkowana',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    ),
+                    const LinearWidgetPointer(
+                      value: 450,
+                      enableAnimation: true,
+                      position: LinearElementPosition.outside,
+                      offset: 4,
+                      child: Text(
+                        'Zła',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    )
+                  ],
+                  ranges: const <LinearGaugeRange>[
+                    LinearGaugeRange(
+                      startValue: 100.0,
+                      endValue: 200,
+                      startWidth: 8,
+                      midWidth: 8,
+                      endWidth: 8,
+                      position: LinearElementPosition.cross,
+                      color: Colors.green,
+                    ),
+                    LinearGaugeRange(
+                      startValue: 200.0,
+                      endValue: 300,
+                      startWidth: 8,
+                      position: LinearElementPosition.cross,
+                      midWidth: 8,
+                      endWidth: 8,
+                      color: Colors.amber,
+                    ),
+                    LinearGaugeRange(
+                      startValue: 300.0,
+                      endValue: 400,
+                      position: LinearElementPosition.cross,
+                      startWidth: 8,
+                      midWidth: 8,
+                      endWidth: 8,
+                      color: Color(0xffFB7D55),
+                    ),
+                    LinearGaugeRange(
+                      startValue: 400.0,
+                      endValue: 500,
+                      position: LinearElementPosition.cross,
+                      startWidth: 8,
+                      midWidth: 8,
+                      endWidth: 8,
+                      color: Colors.red,
+                    ),
+                  ]),
+            ),
+          ],
+        ),
+        Row(
+          children: [
+            Column(
+              //   crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                const Text(
+                  'TODAY',
+                  style: TextStyle(fontSize: 10, fontWeight: FontWeight.w500),
+                ),
+                Text(
+                  _todayValue.toStringAsFixed(0),
+                  style: TextStyle(
+                      fontSize: 26,
+                      color: _todayValue < 200
+                          ? Colors.green
+                          : _todayValue < 300
+                              ? Colors.amber
+                              : _todayValue < 400
+                                  ? const Color(0xffFB7D55)
+                                  : Colors.red,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            SizedBox(
+              child: SfLinearGauge(
+                  minimum: 100.0,
+                  maximum: 500.0,
+                  interval: 50.0,
+                  animateAxis: true,
+                  animateRange: true,
+                  showLabels: false,
+                  showTicks: false,
+                  minorTicksPerInterval: 0,
+                  axisTrackStyle: LinearAxisTrackStyle(
+                    thickness: 15,
+                    color: brightness == Brightness.dark
+                        ? Colors.transparent
+                        : Colors.grey[350],
+                  ),
+                  markerPointers: <LinearMarkerPointer>[
+                    LinearShapePointer(
+                        value: _todayValue,
+                        onChanged: (dynamic value) {
+                          setState(() {
+                            _todayValue = value as double;
+                          });
+                        },
+                        height: 20,
+                        width: 20,
+                        color: _todayValue < 200
+                            ? Colors.green
+                            : _todayValue < 300
+                                ? Colors.amber
+                                : _todayValue < 400
+                                    ? const Color(0xffFB7D55)
+                                    : Colors.red,
+                        position: LinearElementPosition.cross,
+                        shapeType: LinearShapePointerType.circle),
+                    const LinearWidgetPointer(
+                      value: 150,
+                      enableAnimation: true,
+                      position: LinearElementPosition.outside,
+                      offset: 4,
+                      child: Text(
+                        'Doskonała',
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    const LinearWidgetPointer(
+                      value: 250,
+                      enableAnimation: true,
+                      position: LinearElementPosition.outside,
+                      offset: 4,
+                      child: Text(
+                        'Dobra',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    ),
+                    const LinearWidgetPointer(
+                      value: 350,
+                      enableAnimation: true,
+                      position: LinearElementPosition.outside,
+                      offset: 4,
+                      child: Text(
+                        'Umiarkowana',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    ),
+                    const LinearWidgetPointer(
+                      value: 450,
+                      enableAnimation: true,
+                      position: LinearElementPosition.outside,
+                      offset: 4,
+                      child: Text(
+                        'Zła',
+                        style: TextStyle(fontSize: 12),
+                      ),
+                    )
+                  ],
+                  ranges: const <LinearGaugeRange>[
+                    LinearGaugeRange(
+                      startValue: 100.0,
+                      endValue: 200,
+                      startWidth: 8,
+                      midWidth: 8,
+                      endWidth: 8,
+                      position: LinearElementPosition.cross,
+                      color: Colors.green,
+                    ),
+                    LinearGaugeRange(
+                      startValue: 200.0,
+                      endValue: 300,
+                      startWidth: 8,
+                      position: LinearElementPosition.cross,
+                      midWidth: 8,
+                      endWidth: 8,
+                      color: Colors.amber,
+                    ),
+                    LinearGaugeRange(
+                      startValue: 300.0,
+                      endValue: 400,
+                      position: LinearElementPosition.cross,
+                      startWidth: 8,
+                      midWidth: 8,
+                      endWidth: 8,
+                      color: Color(0xffFB7D55),
+                    ),
+                    LinearGaugeRange(
+                      startValue: 400.0,
+                      endValue: 500,
+                      position: LinearElementPosition.cross,
+                      startWidth: 8,
+                      midWidth: 8,
+                      endWidth: 8,
+                      color: Colors.red,
+                    ),
+                  ]),
+            ),
+          ],
+        ),
+      ],
+    );
+  }
+}
 
 class MarkerHelper {
   final MarkerIconLoader _iconLoader;
@@ -27,11 +338,10 @@ class MarkerHelper {
       icon: pm25Avg > 55
           ? _iconLoader.markerIcon4
           : pm25Avg > 35
-          ? _iconLoader.markerIcon3
-          : pm25Avg > 13
-          ? _iconLoader.markerIcon2
-          : _iconLoader.markerIcon,
+              ? _iconLoader.markerIcon3
+              : pm25Avg > 13
+                  ? _iconLoader.markerIcon2
+                  : _iconLoader.markerIcon,
     );
   }
-  
 }
